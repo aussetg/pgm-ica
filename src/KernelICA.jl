4aa7435b126fbb06565508dbd1b9af158717dba7
Guillaume Ausset <guillaume@ausset.me>
2017-01-12 18:45:34 +0100

    Autorise maintenant un contraste arbitraire, un noyau arbitraire et utilise Cholesky lazy

--- src/KernelICA.jl
+++ src/KernelICA.jl
@@ -1,4 +1,5 @@
 include("IncompleteCholesky.jl")
+include("Kernels.jl")
 include("preprocess.jl")
 include("FastICA.jl")
 
@@ -10,31 +11,6 @@ Center in the feature space a gram matrix K given as K = G*G'.
 function gramCentering(G::Matrix{Float64})
   N = size(G,1)
   return G - repmat(mean(G,1),N,1)
-end
-
-"""
-  gramGauss(x::Vector{Float64}[, gamma = 2])
-
-Helper function to create the Gram matrix associated to a Gaussian symmetrix
-kernel and parameter sigma.
-"""
-function gramGauss(x::Vector{Float64}, gamma = 2)
-  N = length(x)
-  K = eye(N)
-  for i = 1:N
-    for j = 1:(i-1)
-      K[i,j] = exp(-gamma*(x[i]-x[j])^2)
-      K[j,i] = K[i,j]
-    end
-  end
-  # TODO: PAGE 11 SAYS WE NEED TO CENTER THE GRAM MATRIX !
-  # TODO: DON'T FORGET TO DO IT
-  N0 = eye(N) - 1/N * ones(N,N)
-  # For some mysterious reasons, N0*K*N0 isn't symetric for Julia.
-  # Probably a numerical problem. So we fix that
-  K = N0*K*N0
-  K = 0.5*(K+K')
-  return K
 end
 
 """
@@ -71,13 +47,13 @@ function regularize(x, N::Int, k = 1e-3)
 end
 
 """
-  updateJ(s, i, j, Rs, Us, Ms, C)
+  updateJ(s, i, j, Rs, Us, Ms, C, K)
 
 This function comute a new contrast (only for KGV at the moment)
 using the fact that the Gram matrix only changes column wise for each
 partial derivative.
 """
-function updateJ(s::Matrix{Float64}, i, j, Rs, Us, Ms, C)
+function updateJ(s::Matrix{Float64}, i, j, Rs, Us, Ms, C::Function, K::Function)
   if (i>j)
     i, j = j, i
   end
@@ -86,11 +62,11 @@ function updateJ(s::Matrix{Float64}, i, j, Rs, Us, Ms, C)
   Usp = Us
   m, N = size(s)
   # First we will compute the updated values for i and j
-  Ki = gramGauss(s[i,:])
-  Kj = gramGauss(s[j,:])
-  Pi, Gi, Mi = IncompleteChol(Ki)
+  Pi, Gi, Mi = IncompleteChol(K, s[i,:])
+  Gi = gramCentering(Gi)
   Msp[i] = Mi
-  Pj, Gj, Mj = IncompleteChol(Ki)
+  Pj, Gj, Mj = IncompleteChol(K, s[j,:])
+  Gj = gramCentering(Gj)
   Msp[j] = Mj
   Ui, Si, Vi = svd(Gi)
   Usp[i] = Ui
@@ -104,7 +80,7 @@ function updateJ(s::Matrix{Float64}, i, j, Rs, Us, Ms, C)
   Rsp[j] = Rj
   # Now we will build Rk
   Rk = eye(sum(Msp))
-  ix = cumsum(Msp) - Msp[1] + 1
+  ix = cumsum(Msp) - Msp + 1
   for i = 2:m
     for j = 1:(i-1)
       mat = Rsp[i] * Usp[i]' * Usp[j] * Rsp[j]
@@ -116,22 +92,22 @@ function updateJ(s::Matrix{Float64}, i, j, Rs, Us, Ms, C)
 end
 
 """
-  finiteD(x,w, C[, ε])
+  finiteD(x,w, C, K[, ε])
 
 This function compute finite differences (only for KGV at the moment)
 using the fact that the Gram matrix only changes column wise for each
 partial derivative.
 """
-function finiteD(x::Matrix{Float64}, w::Matrix{Float64}, C, ε = 1e-3)
+function finiteD(x::Matrix{Float64}, w::Matrix{Float64}, C::Function, K::Function, ε = 1e-3)
   m, N = size(w)
   wd = zeros(m,N)
   s0 = w' * x
-  J0, Rs, Us, Ms = contrast!(s0, C)
+  J0, Rs, Us, Ms = contrast!(s0, C, K)
   for i = 1:(m-1)
     for j = (i+1):m
       s = s0
       s[[i, j],:] = [cos(ε) sin(ε) ; sin(-ε) cos(ε)]*s[[i, j],:]
-      J = updateJ(s, i, j, Rs, Us, Ms, C)
+      J = updateJ(s, i, j, Rs, Us, Ms, C, K)
       wd[i,j] = (J-J0)/ε
       wd[j,i] = -(J-J0)/ε
     end
@@ -140,19 +116,19 @@ function finiteD(x::Matrix{Float64}, w::Matrix{Float64}, C, ε = 1e-3)
 end
 
 """
-  contrast(X, C[, k])
+  contrast(X, C, K[, k])
 
 """
-function contrast(X::Matrix{Float64}, C, k = 2*1e-3)
+function contrast(X::Matrix{Float64}, C::Function, K::Function, k = 2*1e-3)
   m, N = size(X) # Components, Observations
   Ms = zeros(Int,m)
   Rs = Array{Array}(m) # The R matrices
   Us = Array{Array}(m) # The U matrices
   for i = 1:m
-    K = gramGauss(X[i,:])
     # TODO: PAGE 11 SAYS WE NEED TO CENTER THE GRAM MATRIX !
     # TODO: DON'T FORGET TO DO IT
-    P, G, M = IncompleteChol(K)
+    P, G, M = IncompleteChol(K, X[i,:])
+    G = gramCentering(G)
     U,S,V = svd(G)
     L = sparse(diagm(S.^2))
     R = regularize.(L,N,k)
@@ -174,20 +150,20 @@ function contrast(X::Matrix{Float64}, C, k = 2*1e-3)
 end
 
 """
-  contrast!(X, C[, k])
+  contrast!(X, C, K[, k])
 
 Same as kgvc except returns additional informations needed to rebuild Rk
 """
-function contrast!(X::Matrix{Float64}, C, k = 2*1e-3)
+function contrast!(X::Matrix{Float64}, C::Function, K::Function, k = 2*1e-3)
   m, N = size(X) # Components, Observations
   Ms = zeros(Int,m)
   Rs = Array{Array}(m) # The R matrices
   Us = Array{Array}(m) # The U matrices
   for i = 1:m
-    K = gramGauss(X[i,:])
     # TODO: PAGE 11 SAYS WE NEED TO CENTER THE GRAM MATRIX !
     # TODO: DON'T FORGET TO DO IT
-    P, G, M = IncompleteChol(K)
+    P, G, M = IncompleteChol(K, X[i,:])
+    G = gramCentering(G)
     U,S,V = svd(G)
     L = sparse(diagm(S.^2))
     R = regularize.(L,N,k)
@@ -227,24 +203,24 @@ function geod(w::Matrix{Float64}, h::Matrix{Float64}, t)
 end
 
 """
-  initializeSearch(w, d, x, a, b, Ja, C)
+  initializeSearch(w, d, x, a, b, Ja, C, K)
 
 Find inital conditions for the linesearch.
 """
-function initializeSearch(w::Matrix{Float64}, d, x, a, b, Ja, C)
+function initializeSearch(w::Matrix{Float64}, d, x, a, b, Ja, C::Function, K::Function)
   φ = 1.618
   ε = 1e-10
   wp, hp = geod(w,d,a)
-  fa = contrast(wp' * x, K)
+  fa = contrast(wp' * x, C, K)
   wp, hp = geod(w,d,b)
-  fb = contrast(wp' * x, C)
+  fb = contrast(wp' * x, C, K)
   if (fb > fa)
      a, b = b, a
      fa, fb = fb, fa
   end
   c = b + φ * (b-a)
   wp, hp = geod(w,d,c)
-  fc = contrast(wp' * x, C)
+  fc = contrast(wp' * x, C, K)
 
   while (fb > fc)
    r = (b-a)*(fb-fc);
@@ -253,7 +229,7 @@ function initializeSearch(w::Matrix{Float64}, d, x, a, b, Ja, C)
    ulim = b+100*(c-b)
    if ((b-u)*(u-c) > 0)
       wp, hp = geod(w,d,u)
-      fu = contrast(wp'*x, C)
+      fu = contrast(wp'*x, C, K)
 
       if (fu < fc)
          a = b
@@ -270,11 +246,11 @@ function initializeSearch(w::Matrix{Float64}, d, x, a, b, Ja, C)
       end
       u = c + 100*(c-b)
       wp, hp = geod(w,d,u)
-      fu = contrast(wp'*x, C)
+      fu = contrast(wp'*x, C, K)
    else
       if ((c-u)*(u-ulim) > 0)
          wp, hp = geod(w,d,u)
-         fu = contrast(wp'*x, C)
+         fu = contrast(wp'*x, C, K)
          if (fu < fc)
             b = c
             c = u
@@ -283,19 +259,19 @@ function initializeSearch(w::Matrix{Float64}, d, x, a, b, Ja, C)
             fb = fc
             fc = fu
             wp, hp = geod(w,d,u)
-            fu = contrast(wp'*x, C)
+            fu = contrast(wp'*x, C, K)
          end
       else
          if ((u-ulim)*(ulim-c) >= 0)
 
             u = ulim
             wp, hp = geod(w,d,u)
-            fu = contrast(wp'*x, C)
+            fu = contrast(wp'*x, C, K)
 
          else
             u = c + 100*(c-b)
             wp, hp = geod(w,d,u)
-            fu = contrast(wp'*x, C)
+            fu = contrast(wp'*x, C, K)
          end
       end
    end
@@ -306,15 +282,15 @@ return a,b,c,fa,fb,fc
 end
 
 """
-  linesearch(w, d, x, a, b, c, C[, tol = 1e-2])
+  linesearch(w, d, x, a, b, c, C, K[, tol = 1e-2])
 
 Perform a linesearch by golden section.
 Not exactly a linesearch per se, but equivalent on a geodesic of the
 Stiefel manifold.
 """
-function linesearch(w::Matrix{Float64}, d, x, a, b, c, C, tol = 1e-2)
+function linesearch(w::Matrix{Float64}, d, x, a, b, c, C::Function, K::Function, tol = 1e-2)
   φ = 1.618
-  C = 0.382
+  p = 0.382
   R = 0.618
   resφ = 0.382
   x0 = a
@@ -323,31 +299,31 @@ function linesearch(w::Matrix{Float64}, d, x, a, b, c, C, tol = 1e-2)
 
   if (abs(c-b) > abs(b-a))
     x1 = b
-    x2 = b + C*(c-b)
+    x2 = b + p*(c-b)
   else
     x2 = b
-    x1 = b - C*(b-a)
+    x1 = b - p*(b-a)
   end
   wp, hp = geod(w, d, x1)
-  f1 = contrast(wp'*x, C)
+  f1 = contrast(wp'*x, C, K)
   wp, hp = geod(w, d, x2)
-  f2 = contrast(wp'*x, C)
+  f2 = contrast(wp'*x, C, K)
   k = 1
   while ((abs(x3-x0) > tol) & (k<maxiter))
     if f2 < f1
       x0 = x1
       x1 = x2
-      x2 = R*x1 + C*x3
+      x2 = R*x1 + p*x3
       f1 = f2
       wp, hp = geod(w,d,x2)
-      f2 = contrast(wp'*x, C)
+      f2 = contrast(wp'*x, C, K)
     else
       x3 = x2
       x2 = x1
-      x1 = R*x2 + C*x0
+      x1 = R*x2 + p*x0
       f2 = f1
       wp, hp = geod(w, d, x1)
-      f1 = contrast(wp'*x, C)
+      f1 = contrast(wp'*x, C, K)
     end
     k = k+1;
   end
@@ -362,36 +338,39 @@ function linesearch(w::Matrix{Float64}, d, x, a, b, c, C, tol = 1e-2)
 end
 
 """
-  kica(X, C)
+  kica(X,[C = kgv, K = gaussian])
 
 Perform ICA using the contrast function C and using steepest descent.
+The contrast is computed with respect to the kernel K of signature:
+K :: Float -> Float -> Float
 """
-function kica(x::Matrix{Float64}, C::Function)
+function kica(x::Matrix{Float64}, C::Function = kgv, K::Function = gaussian)
   m, N = size(x)
   # For our intitial guess we use FastICA
   w, _ = fastICA(x,m)
+  w = w'
   ε = 1e-5
   err = 1
   maxiter = 15
   iters = 0
   b = 1
   while (err > ε) && iters < maxiter
-    J, grad = finiteD(x, w, C)
+    J, grad = finiteD(x, w, C, K)
     d = grad
 
     gap = sqrt(0.5*trace(grad'*grad))
 
     ## Initialize interval
-    a,b,c,fa,fb,fc = initializeSearch(w, d, x, 0, b, J, C)
+    a,b,c,fa,fb,fc = initializeSearch(w, d, x, 0, b, J, C, K)
     tol=max(abs(ε/gap), abs(mean([a, b, c])/10))
     ## Search on the interval
-    b, Jmin = linesearch(w, d, x, a, b, c, tol, C)
+    b, Jmin = linesearch(w, d, x, a, b, c, C, K, tol)
     w, h = geod(w,d,b)
     err = abs(Jmin/J - 1)
     iters = iters + 1
     J = Jmin
   end
-  return w, w'*xw
+  return w, w'*x
 end
 
 """
